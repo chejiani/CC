e63ed6a685a57e251409e95f12a12f36d6474fb3
自东.刘 <spnor.re@gmail.com>
2019-01-15 15:29:01 +0800

    更新数据库配置

--- src/main/java/com/ccut/ytzhuang/SpringBootstrap.java
+++ src/main/java/com/ccut/ytzhuang/SpringBootstrap.java
@@ -1,12 +1,8 @@
 package com.ccut.ytzhuang;
 
-import freemarker.template.Configuration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @SpringBootApplication
 @EnableTransactionManagement
